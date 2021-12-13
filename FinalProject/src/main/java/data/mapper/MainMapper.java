a794e418b148d0b196c511605767a99cbbeb06cd
gungs <gungs@DESKTOP-VS2TFJK>
2021-12-13 23:49:01 +0900

    1213.2348

--- FinalProject/src/main/java/data/mapper/MainMapper.java
+++ FinalProject/src/main/java/data/mapper/MainMapper.java
@@ -22,10 +22,9 @@ public interface MainMapper {
 	public int getCartCount(String user_id);
 
 	public List<StoryDto> getMainStoryList();
+	public int getStoryTotalCount();
 
 	//검색 기능
 	public List<ClassBoardDto> getSearch(HashMap<String, Object> map);
 	public int getSearchTotal();
-
-	public int getStoryTotalCount();
 }
