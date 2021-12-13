7e4c5dadc84401b83ad1ecf24ce9f085a47e39e5
yuninjeong <yijeong0918@gmail.com>
2021-12-13 13:55:02 +0900

    modify: yij

--- FinalProject/src/main/java/data/mapper/UserMapper.java
+++ FinalProject/src/main/java/data/mapper/UserMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package data.mapper;
 
 import java.util.HashMap;
@@ -32,37 +31,4 @@ public interface UserMapper {
 	
 	public List<UserDto> getUsersData();
 }
-=======
-package data.mapper;
 
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-import data.dto.UserDto;
-
-
-@Mapper
-public interface UserMapper {
-
-	public UserDto GetIdData(String myid);
-  	public int getTotalCount(String myid);
-  	public void insertUsers(UserDto dto);
-  	public int getIdCheck(String id);
-  	public int getNickCheck(String nickname);
-  	public int getEmailCheck(String email);
-	public int getCheckPass(HashMap<String, String> map);
-	public UserDto getUserData(String id);
-	public void deleteUsers(String id);
-	public void updateUsers(UserDto dto);
-	public void updatePass(UserDto dto);
-	
-	public String getNickName(String id);
-	public String getCategory(String id);
-	public String getEmailId(String email);
-	
-	public int login(HashMap<String, String> map);
-	
-	public List<UserDto> getUsersData();
-}
->>>>>>> branch 'main' of https://github.com/chominsub/WeCList.git
