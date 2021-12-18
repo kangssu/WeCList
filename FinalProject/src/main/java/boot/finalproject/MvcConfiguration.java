1941080dc3eabadb30c3bdad2061fa246eaaec44
vz4repo <50684258+vz4repo@users.noreply.github.com>
2021-12-18 17:02:39 +0900

    PR: change image uplaod path (#32)
    fix: image upload path as UNIX style

--- FinalProject/src/main/java/boot/finalproject/MvcConfiguration.java
+++ FinalProject/src/main/java/boot/finalproject/MvcConfiguration.java
@@ -1,9 +1,17 @@
 package boot.finalproject;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+
+  private final String imagePath;
+  public MvcConfiguration(@Value("${custom.path.images}") String imagePath){
+    this.imagePath = imagePath;
+  }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -19,5 +27,8 @@ public class MvcConfiguration implements WebMvcConfigurer {
     /* '/fonts/**'로 호출하는 자원은 '/static/fonts/' 폴더 아래에서 찾는다. */
     registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/")
         .setCachePeriod(60 * 60 * 24 * 365);
+    /* '/photo/**' 로 호출하는 자원은 '~/backup/photo/' (리눅스)폴더 아래에서 찾는다. */
+    registry.addResourceHandler("/photo/**").addResourceLocations("file:/home/ec2-user/backup/photo")
+        .setCachePeriod(3600);
   }
 }
